996d3b835187f93dccf0ee76440bd77cda2ce5f4
c3ma <c3ma@calculator>
2014-06-21 21:32:53 +0200

    Included a tool to press a button

--- jni/main.c
+++ jni/main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define CMD_LENGTH	32
+
+static int click(int x, int y)
+{
+	int res, err;
+	char cmd[CMD_LENGTH];
+
+	memset(cmd, 0, CMD_LENGTH);
+	sprintf(cmd, "input touchscreen tap %d %d", x , y);
+	fprintf(stdout,"Cmd is %s\n", cmd); /* DEBUG */
+	res = system(cmd);
+	fprintf(stdout,"execution returned %d.\n",res);
+	if ((-1 != res) && (127 != res))
+		fprintf(stdout,"now would be a good time to check out 'man split' to check what the resulting return-value (%d) means.\n",res);
+	return res;
+}
 
 int main()
 {
 	printf("Hello World\n");
+	click(120, 120);
 	return 0;
 }
