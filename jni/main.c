350188c6eeff842121e080fee1d301daa937488d
c3ma <c3ma@calculator>
2014-06-21 21:46:14 +0200

    Some found ideas stored

--- jni/main.c
+++ jni/main.c
@@ -3,6 +3,7 @@
 
 #define CMD_LENGTH	32
 
+
 static int click(int x, int y)
 {
 	int res, err;
@@ -23,3 +24,28 @@ int main()
 	click(120, 120);
 	return 0;
 }
+
+/** Idea pool:
+ *  cat /dev/input/mice | od -t x1 -w3
+ *
+ * Improved python version:
+ * import struct
+
+file = open( "/dev/input/mice", "rb" );
+
+def getMouseEvent():
+  buf = file.read(3);
+  button = ord( buf[0] );
+  bLeft = button & 0x1;
+  bMiddle = ( button & 0x4 ) > 0;
+  bRight = ( button & 0x2 ) > 0;
+  x,y = struct.unpack( "bb", buf[1:] );
+  print ("L:%d, M: %d, R: %d, x: %d, y: %d\n" % (bLeft,bMiddle,bRight, x, y) );
+  # return stuffs
+
+while( 1 ):
+  getMouseEvent();
+file.close();
+
+ *
+ */
